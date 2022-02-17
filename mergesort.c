8019989d9370cb38b8bea1abfb2dffadd3bb35b7
Daniel Partika <72474345+DanPartika@users.noreply.github.com>
2022-02-17 18:47:01 -0500

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,1 +1,25 @@
-here
+#include <stdio.h>
+#include <stdlib.h>
+#include "mergesort.h"
+
+int int_cmp(const void *a, const void *b){
+    if ( *(int *) a > *(int *) b ) {
+        return 1;
+    } else if ( *(int *) a < *(int *) b ) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+int dbl_cmp(const void *a, const void *b){
+    if ( *(double *) a > *(double *) b ) {
+        return 1;
+    } else if ( *(double *) a < *(double *) b ) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+//Mergesort
