31e9ef49ee622a7bb4c75e5ae973c72150c198ce
Daniel Partika <72474345+DanPartika@users.noreply.github.com>
2022-02-18 21:25:28 -0500

    Update sort.c

--- sort.c
+++ sort.c
@@ -2,82 +2,89 @@
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
-#include "mergesort.h"
+#include "mergesort.c"
 
-
+//usage function that just prints the usage.
 void usage(){
-    printf("Usage: ./sort [-i][-d] filename\n");
-    printf("-i: Specifies the file containing ints.\n");
+    printf("Usage: ./sort [-i|-d] filename\n");
+    printf("-i: Specifies the file contains ints.\n");
     printf("-d: Specifies the file contains doubles.\n");
     printf("filename: The file to sort.\n");
 }
 
 int main(int argc, char *argv[]) {
-//216940
-//160757'
+    int mode = 0; //used to see if file has ints or doubles
+    int check = 0; //used to error check for too many valid flags
     
-    if (argc == 1){
+    if (argc == 1){ //no arguements
         usage();
         return EXIT_FAILURE;
-    } else if(*(argv[1]) == '-' && argc == 2) {
-        printf("Error: No input file specified.");
-        return EXIT_FAILURE;
-
     } else {
-        int opt;
-        int mode = 0;
-        while ((opt = getopt(argc, argv, "id")) != -1) {
+        int opt; //used for getopt loop
+        while ((opt = getopt(argc, argv, ":id")) != -1) {
             switch (opt) {
-            case 'i':
-                mode =1;
-                if (argc == 2){
+                case 'i':
+                    if (argc == 2){ // checks to see if there is a file after the flag
+                        printf("Error: No input file specified.\n");
+                        return EXIT_FAILURE;
+                    } 
+                    mode = 1; //mode = ints
+                    check++;
+                    break;
+                case 'd':
+                    if (argc == 2){ // checks to see if there is a file after the flag
+                        printf("Error: No input file specified.\n");
+                        return EXIT_FAILURE;
+                    } 
+                    mode = 2; //mode = doubles
+                    check++;
+                    break;
+                default:
+                    fprintf(stderr, "Error: Uknown option '-%c' recieved.\n", optopt);
                     usage();
-                } 
-                break;
-            case 'd':
-            mode=2;
-                if (argc == 2){
-                    usage();
-                } 
-                break;
-            default:
-                fprintf(stderr, "Error: Uknown option '-%s' recieved\n", argv[1]);
-                usage();
-                return EXIT_FAILURE;
-                break;
+                    return EXIT_FAILURE;
+                    break;
             }
 
         }
     }
-
-    FILE *file = fopen(argv[2], "r");
-    if (file == NULL) {
-        printf("Error: Cannot open '%s'. No such file or directory.\n", file);
+    if (check > 1){ //checks to see if there is only one valid flag
+        printf("Error: Too many flags specified.\n");
+        return EXIT_FAILURE;
+    }
+    //at this point no errors in flags so check to see if theres only one file
+    if (argc > 3){ 
+        printf("Error: Too many files specified.\n");
+        return EXIT_FAILURE;
+    }
+    //Opening the file
+    FILE* file = fopen(argv[2], "r");
+    if (file == NULL) { //checks to see if it is empty
+        printf("Error: Cannot open '%s'. No such file or directory.\n", argv[2]);
         return EXIT_FAILURE;
     }
     void* ptr; //points to int array to pass to mergesort
-    size_t c = 0;//len
+    size_t len = 0;//len
     size_t elem_sz=0;//elemsz
-    int (pintr*)(const void*, const void*); //pointer to int/dbl_cmp
+    int (*pintr)(const void*, const void*); //pointer to int/dbl_cmp
     
     if (mode == 1) {  //ints
         //declare int array and count
         
         int* array = (int*)malloc(sizeof(int)*1024);
 
-        while( fscanf(file, "%d",array[c]) != EOF){
-            c++;
+        while( fscanf(file, "%d\n",&array[len]) != EOF){
+            len++;
             //set up mergesort parameters for int
-            
         }
         ptr = array;
         elem_sz = 4; 
         pintr = &int_cmp;
-    } else if (mode == 2){ //doubles
-        double array[1024];
+    } else if (mode == 2) { //doubles
+        double* array = (double*)malloc(sizeof(double)*1024); 
 
-        while( fscanf(file, "%lf",array[c]) != EOF){
-            c++;
+        while( fscanf(file, "%lf\n",&array[len]) != EOF){
+            len++;
             //set up mergesort parameters for int
             
         }
@@ -85,19 +92,22 @@ int main(int argc, char *argv[]) {
         elem_sz = 8;
         pintr=&dbl_cmp;
     }
-    mergesort(ptr,c,elem_sz,pintr);
+    //for(size_t i = 0; i < len; i++){
+    //    printf("%d\n", *((int*)ptr+i));
+    //}
+    mergesort(ptr,len,elem_sz,pintr);
     //print the sorted array
-
-    if (mode == 1) {
-        for(size_t i = 0; i < c; i++){
-            printf("%d", *((int*)ptr+i);
+    if (mode == 1) { //print int array
+        for(size_t i = 0; i < len; i++){
+            printf("%d\n", *((int*)ptr+i));
         }
-    } else if (mode == 2) {
-        for(size_t i = 0; i < c; i++){
-            printf("%lf", *((double*)ptr+i);
+    } else if (mode == 2) { //print double array
+        for(size_t i = 0; i < len; i++){
+            printf("%lf\n", *((double*)ptr+i));
         }
     }
-    free(array);
-    fclose(file);
-    return EXIT_SUCCESS;
+
+    free(ptr);  //free dynamically allocated memory
+    fclose(file); //close the file
+    return EXIT_SUCCESS; //EXIT SUCCESS!
 }
